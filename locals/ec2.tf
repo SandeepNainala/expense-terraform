59f46454aa34ad5ec9d6584fb3a8cff80674b7cd
sandeepnaidu <nainala_sandeep@yahoo.com>
2024-05-10 21:50:34 +0530

    terraform - locals

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "db" {
   ami          = local.ami_id
-  instance_type = var.instance_type == "db" ? "t3.small" : "t3.micro"
+  instance_type = local.instance_type
   vpc_security_group_ids = local.sg_id
   tags = local.tags
 }
--- locals/locals.tf
+++ locals/locals.tf
@@ -1,6 +1,6 @@
 locals {
   ami_id = "ami-090252cbe067a9e58"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type == "db" ? "t3.small" : "t3.micro"
   sg_id = "sg-052d406550420b45d"
   tags = {
     name = "db"
