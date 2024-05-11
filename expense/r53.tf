92329a80ed47d2bab6b850cf7fc226632ef152e0
sandeepnaidu <nainala_sandeep@yahoo.com>
2024-05-11 11:28:49 +0530

    terraform - locals expense project

--- expense/r53.tf
+++ expense/r53.tf
@@ -1,9 +1,11 @@
 resource "aws_route53_record" "expense" {
   count = length(var.instance_name)
   zone_id = var.instance_name
-  name    = var.instance_name[count.index] == "frontend" ? var.domain_name : "${var.instance_name[count.index]}.${var.domain_name}"
+  #name    = var.instance_name[count.index] == "frontend" ? var.domain_name : "${var.instance_name[count.index]}.${var.domain_name}"
+  name = local.record_name
   type    = "A"
   ttl     = 300
-  records = var.instance_name[count.index] == frontend ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
+  #records = var.instance_name[count.index] == frontend ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
+  records = local.record_value
   allow_overwrite = true
 }
