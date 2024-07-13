30b0c5108300f5726e5d6d479e894fe64d726c5a
sandeepanainala <nainala_sandeep@yahoo.com>
2024-07-13 13:07:42 +0530

    terraform workstation

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "db" {
-  ami           = ami-090252cbe067a9e58
+  ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   user_data = file("eks.sh")
