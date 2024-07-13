03602cecee58c890d72af61a1227d1cfcc974574
sandeepanainala <nainala_sandeep@yahoo.com>
2024-07-13 12:58:08 +0530

    terraform workstation

---
+++ workstation/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "workstation" {
+  ami           = ami-041e2ea9402c46c32
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "workstation"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+    CreatedBy = "Sandeep"
+  }
+}
