8072a277c988edead80365e061f165ac1855a6b7
sandeepanainala <nainala_sandeep@yahoo.com>
2024-07-13 13:04:56 +0530

    terraform workstation

---
+++ ec2/eks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#!/bin/bash
+# Update the package repository
+sudo yum update -y
+
+# Install AWS CLI v2
+sudo yum install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Install kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+
+# Verify installation
+eksctl version
+kubectl version --client
+
+
